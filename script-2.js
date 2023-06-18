413d4fd1736d9a8b469ca3bbcc658aa6f6478f76
AntiJ-RTC <103210593+AntiJ-RTC@users.noreply.github.com>
2023-06-17 19:26:28 -0700

    more pages + localstorage

--- script-2.js
+++ script-2.js
@@ -34,54 +34,54 @@ async function getWeather(city){
     const apiKey ='72cf5f0ea187d5f8386c5cd9bc9f3e06';
 
     console.log(city);
-    localStorage.setItem('city', JSON.stringify(city));
+    localStorage.setItem('city2', JSON.stringify(city));
     const lat = city.latitude;
     const lon = city.longitude;
     console.log(`${lat} ${lon}`)
     const weatherRes = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
     const weatherData = await weatherRes.json();
-    localStorage.setItem('weather', JSON.stringify(weatherData));
+    localStorage.setItem('weather2', JSON.stringify(weatherData));
     console.log(weatherData);
 
     const forecastRes = await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`);
     const forecastData = await forecastRes.json();
-    localStorage.setItem('forecast', JSON.stringify(forecastData));
+    localStorage.setItem('forecast2', JSON.stringify(forecastData));
     console.log(forecastData);
 }
 
 function loadWeather(){
     window.addEventListener('keyup', function(evt){
         if(evt.key == "Enter"){
-            this.window.location.href = "single-forecast.html"
+            this.window.location.href = "dual-forecast.html"
         }
     });
-    const city = JSON.parse(localStorage.getItem('city'));
-    const weatherData = JSON.parse(localStorage.getItem('weather'));
-    const forecastData = JSON.parse(localStorage.getItem('forecast'));
+    const city = JSON.parse(localStorage.getItem('city2'));
+    const weatherData = JSON.parse(localStorage.getItem('weather2'));
+    const forecastData = JSON.parse(localStorage.getItem('forecast2'));
 
     console.log(weatherData);
     console.log(forecastData);
 
-    let miniWeather = document.querySelector('.mini-weather');
+    let miniWeather2 = document.querySelector('.mini-weather-2');
 
-    let cityName = miniWeather.querySelector('.city-title h1');
+    let cityName = miniWeather2.querySelector('.city-title h1');
     cityName.innerHTML = `${city.name}, ${city.admin1}, ${city.country}`;
 
     const unixTime = weatherData.dt * 1000;
     const unixToDate = new Date(unixTime).toLocaleString('en-US');
-    miniWeather.querySelector('.city-title h3').innerHTML = `Last Updated: ${unixToDate}`
+    miniWeather2.querySelector('.city-title h3').innerHTML = `Last Updated: ${unixToDate}`
 
     let weatherIcon = document.createElement('img')
     weatherIcon.src = `https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
-    let src = miniWeather.querySelector('.weather-icon');
+    let src = miniWeather2.querySelector('.weather-icon');
     src.appendChild(weatherIcon);
 
-    miniWeather.querySelector('.current-temp').innerHTML = `${parseInt(weatherData.main.temp)}&deg;F`;
-    miniWeather.querySelector('.low-temp').innerHTML = `Low: ${parseInt(weatherData.main.temp_min)}&deg;F`;
-    miniWeather.querySelector('.high-temp').innerHTML = `High: ${parseInt(weatherData.main.temp_max)}&deg;F`;
-    miniWeather.querySelector('.weather-type').innerHTML = `${weatherData.weather[0].main}`;
-    miniWeather.querySelector('.humidity').innerHTML = `Humidity: ${weatherData.main.humidity}%`;
-    miniWeather.querySelector('.wind').innerHTML = `Wind: ${weatherData.wind.speed} mph`;
+    miniWeather2.querySelector('.current-temp').innerHTML = `${parseInt(weatherData.main.temp)}&deg;F`;
+    miniWeather2.querySelector('.low-temp').innerHTML = `Low: ${parseInt(weatherData.main.temp_min)}&deg;F`;
+    miniWeather2.querySelector('.high-temp').innerHTML = `High: ${parseInt(weatherData.main.temp_max)}&deg;F`;
+    miniWeather2.querySelector('.weather-type').innerHTML = `${weatherData.weather[0].main}`;
+    miniWeather2.querySelector('.humidity').innerHTML = `Humidity: ${weatherData.main.humidity}%`;
+    miniWeather2.querySelector('.wind').innerHTML = `Wind: ${weatherData.wind.speed} mph`;
     
 }
 
